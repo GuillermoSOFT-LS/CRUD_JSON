961d449eff965556ca0a3a148b7fe78e49e11c06
GuillermoSOFT-LS <guillermolarasoft@gmail.com>
2025-07-15 16:46:33 -0400

    Se realizo exitosamente la llamada  a la api

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,27 @@
 import { Text, View } from "react-native";
+import {useEffect, useState} from "react";
 
 export default function Index() {
+
+    const [dateUsers, setDateUsers] = useState()
+
+    useEffect(() => {
+        const ApiDate = async () => {
+          const response =  await fetch('https://api.jsonbin.io/v3/b/6876b5312a92ba0befe79fe5', {
+              method: 'GET',
+              headers:{
+                  'X-Master-Key': '$2a$10$fi1G/pWAoWakEB5uw3HLI.9IYWfsd7si3QkDyvtv2kag9HxuXfqJC',
+                  'content-type': 'application/json'
+              }
+          })
+            const date = await response.json()
+            setDateUsers(date)
+        }
+        ApiDate();
+    }, []);
+
+
+
   return (
     <View
       style={{
@@ -9,7 +30,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text className='text-3xl'>Edit app/index.tsx to edit this screen.</Text>
+      <Text className=''>{JSON.stringify(dateUsers,null,2)}</Text>
     </View>
   );
 }
